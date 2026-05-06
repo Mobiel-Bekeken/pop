677fa4808d592a27a8238d77a231f71b7d68eae9
Thijs Ubbink | Mobiel Bekeken <thijs@mobielbekeken.nl>
2026-05-06 11:16:53 +0200

    fixes

--- Package.swift
+++ Package.swift
@@ -19,11 +19,40 @@ let package = Package(
     targets: [
         .target(
             name: "pop",
-            dependencies: [
-                
+            path: ".",
+            exclude: [
+                "Configuration",
+                "Images",
+                "Pods",
+                "pop-tests",
+                "pop.xcodeproj",
+                "pop.xcworkspace"
             ],
-            path: "pop",
-            publicHeadersPath: "."
+            sources: [
+                "pop/POPAnimatableProperty.mm",
+                "pop/POPAnimation.mm",
+                "pop/POPAnimationEvent.mm",
+                "pop/POPAnimationExtras.mm",
+                "pop/POPAnimationRuntime.mm",
+                "pop/POPAnimationTracer.mm",
+                "pop/POPAnimator.mm",
+                "pop/POPBasicAnimation.mm",
+                "pop/POPCGUtils.mm",
+                "pop/POPCustomAnimation.mm",
+                "pop/POPDecayAnimation.mm",
+                "pop/POPGeometry.mm",
+                "pop/POPLayerExtras.mm",
+                "pop/POPMath.mm",
+                "pop/POPPropertyAnimation.mm",
+                "pop/POPSpringAnimation.mm",
+                "pop/POPVector.mm",
+                "pop/WebCore/TransformationMatrix.cpp"
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include"),
+                .headerSearchPath("pop/WebCore")
+            ]
         )
     ]
 )
